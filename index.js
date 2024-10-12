3830c023374049447fb7b3d819a72838c91a27d6
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-10-13 10:38:30 +1300

    changed port

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ const httpsServer = https.createServer({
   cert: fs.readFileSync('/etc/letsencrypt/live/corbenykt.ru/fullchain.pem'),
 }, app);
 
-httpsServer.listen(80, () => {
-  console.log('HTTPS Server running on port 80');
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
 });
 
 
