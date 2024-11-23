a6b33274c643e506ee427ab0154b4a1acb167739
CorbenYkt <117908636+CorbenYkt@users.noreply.github.com>
2024-11-24 10:23:01 +1300

    deleted all trash

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 This repository contains the API for the main website https://corbenykt.github.io/movieadvisor. The API is built using Node.js and Express and handles several requests:
 
 ```javascript
-app.post('/auth/register', RegisterValidation, UserController.Register);
 app.get('/likes', LikeController.getAll);
 app.get('/dolike', LikeController.doLike);
 app.post('/likes', LikeController.create);
@@ -12,4 +11,5 @@ app.delete('/likes', LikeController.remove);
 ```
 The functionality is straightforward: the frontend is hosted on GitHub, the API is deployed on an AWS EC2 server, and MongoDB is used to save users' favorite movies.
 
-The server runs on port 443 and uses an SSL certificate. This setup is necessary because GitHub, which hosts the frontend, also uses a certificate. To ensure secure communication, frontend requests to the backend are encrypted.
+The AWS EC2 server runs on port 443 and uses an SSL certificate. This setup is necessary because GitHub, which hosts the frontend, also uses a certificate. To ensure secure communication, frontend requests to the backend are encrypted.
+
--- controlles/LikeController.js
+++ controlles/LikeController.js
@@ -3,28 +3,11 @@ import LikeModel from '../models/Like.js';
 export const getAll = async (req, res) => {
     try {
         const likes = await LikeModel.find().populate('userid').exec();
-
         res.json(likes)
     } catch (err) {
         console.log(err)
         res.status(500).json({
             message: 'Error fetching all likes'
-        })
-    }
-};
-export const getOne = async (req, res) => {
-    try {
-        const postId = req.params.id;
-        const post = await PostModel.findById({
-            _id: postId
-        }).populate('user').exec();
-
-        res.json(post)
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: 'Error fetching one post'
         })
     }
 };
@@ -45,6 +28,23 @@ export const doLike = async (req, res) => {
         console.log(err)
         res.status(500).json({
             message: 'Error fetching does user like'
+        })
+    }
+};
+
+export const create = async (req, res) => {
+    try {
+        const doc = new LikeModel({
+            movieid: req.body.movieid,
+            userid: req.body.userid
+        })
+        const like = await doc.save();
+        res.json(like);
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Error creating like to movie'
         })
     }
 };
@@ -81,23 +81,6 @@ export const remove = async (req, res) => {
         console.log(err)
         res.status(500).json({
             message: 'Error removing post'
-        })
-    }
-};
-
-export const create = async (req, res) => {
-    try {
-        const doc = new LikeModel({
-            movieid: req.body.movieid,
-            userid: req.body.userid
-        })
-        const like = await doc.save();
-        res.json(like);
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Error creating like to movie'
         })
     }
 };
